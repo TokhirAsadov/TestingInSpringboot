285f5311bb69f1622e5f5f08f95c37713efdc4b1
Takhir <guvalakat1603@gmail.com>
2024-07-30 14:10:35 +0500

    cause of difficulty

--- MockitoTestingDemo/src/test/java/uz/tokhir/mockitotestingdemo/business/SomeBusinessImplTest.java
+++ MockitoTestingDemo/src/test/java/uz/tokhir/mockitotestingdemo/business/SomeBusinessImplTest.java
@@ -8,17 +8,33 @@ public class SomeBusinessImplTest {
 
     @Test
     void findTheGreatestFromAllData_basicScenario() {
-        DataServiceStub dataServiceStub = new DataServiceStub();
+        DataServiceStub1 dataServiceStub = new DataServiceStub1();
         SomeBusinessImpl business = new SomeBusinessImpl(dataServiceStub);
         int result = business.findTheGreatestFromAllData();
         assertEquals(25,result);
     }
+
+    @Test
+    void findTheGreatestFromAllData_withOneValue() {
+        DataServiceStub2 dataServiceStub = new DataServiceStub2();
+        SomeBusinessImpl business = new SomeBusinessImpl(dataServiceStub);
+        int result = business.findTheGreatestFromAllData();
+        assertEquals(45,result);
+    }
 }
 
-class DataServiceStub implements DataService{
+class DataServiceStub1 implements DataService{
 
     @Override
     public int[] retrieveAllData() {
         return new int[]{25,15,5};
+    }
+}
+
+class DataServiceStub2 implements DataService{
+
+    @Override
+    public int[] retrieveAllData() {
+        return new int[]{45};
     }
 }
