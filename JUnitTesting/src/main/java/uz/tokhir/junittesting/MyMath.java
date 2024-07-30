2198d8c8e03f4bb2ce52bc3c5407f05977830c37
Takhir <guvalakat1603@gmail.com>
2024-07-30 12:28:20 +0500

    testing files

---
+++ JUnitTesting/src/main/java/uz/tokhir/junittesting/MyMath.java
@@ -0,0 +1,13 @@
+package uz.tokhir.junittesting;
+
+public class MyMath {
+
+    public int calculateSum(int[] numbers){
+        int sum = 0;
+        for (int number : numbers) {
+            sum += number;
+        }
+        return sum;
+    }
+
+}
---
+++ JUnitTesting/src/test/java/uz/tokhir/junittesting/MyMathTest.java
@@ -0,0 +1,20 @@
+package uz.tokhir.junittesting;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class MyMathTest {
+
+    @Test
+    void test() {
+
+        int[] numbers = {1,2,3};
+        MyMath math = new MyMath();
+        int result = math.calculateSum(numbers);
+        System.out.println(result);
+        int expectedResult = 6;
+        assertEquals(expectedResult, result);
+
+    }
+}
