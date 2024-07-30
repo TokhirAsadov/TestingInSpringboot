5ac5698c1e53973767dcfd8d8980a8c98a9342e3
Takhir <guvalakat1603@gmail.com>
2024-07-30 13:40:15 +0500

    @BeforeEach, @AfterEach, @BeforeAll, @AfterAll annotation are used

---
+++ JUnitTesting/src/test/java/uz/tokhir/junittesting/MyBeforeAfterTest.java
@@ -0,0 +1,41 @@
+package uz.tokhir.junittesting;
+
+import org.junit.jupiter.api.*;
+
+public class MyBeforeAfterTest {
+
+    @BeforeAll
+    static void beforeAll(){
+        System.out.println("Before All");
+    }
+
+    @BeforeEach
+    void beforeEach(){
+        System.out.println("Before Each");
+    }
+
+    @Test
+    void test1() {
+        System.out.println("test1");
+    }
+
+    @Test
+    void test2() {
+        System.out.println("test2");
+    }
+
+    @Test
+    void test3() {
+        System.out.println("test3");
+    }
+
+    @AfterEach
+    void afterEach(){
+        System.out.println("After Each");
+    }
+
+    @AfterAll
+    static void afterAll(){
+        System.out.println("After All");
+    }
+}
