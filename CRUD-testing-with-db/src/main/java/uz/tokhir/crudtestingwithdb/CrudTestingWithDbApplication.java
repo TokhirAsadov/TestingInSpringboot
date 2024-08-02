2100330ba4606da91f3fa4ebdfb267e2544f3433
Takhir <guvalakat1603@gmail.com>
2024-08-02 16:55:11 +0500

    crud testing module is added

---
+++ CRUD-testing-with-db/src/main/java/uz/tokhir/crudtestingwithdb/CrudTestingWithDbApplication.java
@@ -0,0 +1,13 @@
+package uz.tokhir.crudtestingwithdb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudTestingWithDbApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CrudTestingWithDbApplication.class, args);
+    }
+
+}
---
+++ CRUD-testing-with-db/src/test/java/uz/tokhir/crudtestingwithdb/CrudTestingWithDbApplicationTests.java
@@ -0,0 +1,13 @@
+package uz.tokhir.crudtestingwithdb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudTestingWithDbApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
