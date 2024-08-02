3906f74bf316476c2451cea7ca2b7de9a277a225
Takhir <guvalakat1603@gmail.com>
2024-08-02 16:58:48 +0500

    @PostConstruct is used

--- CRUD-testing-with-db/src/main/java/uz/tokhir/crudtestingwithdb/CrudTestingWithDbApplication.java
+++ CRUD-testing-with-db/src/main/java/uz/tokhir/crudtestingwithdb/CrudTestingWithDbApplication.java
@@ -1,10 +1,27 @@
 package uz.tokhir.crudtestingwithdb;
 
+import jakarta.annotation.PostConstruct;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import uz.tokhir.crudtestingwithdb.repository.ProductRepository;
 
 @SpringBootApplication
 public class CrudTestingWithDbApplication {
+
+    @Autowired
+    private ProductRepository repository;
+
+    @PostConstruct
+    public void init() {
+//        repository.saveAll(
+//                Stream
+//                        .of(
+//                                new Product("Book", 1, 299),
+//                                new Product("mobile", 1, 39999))
+//                        .collect(Collectors.toList())
+//        );
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(CrudTestingWithDbApplication.class, args);
