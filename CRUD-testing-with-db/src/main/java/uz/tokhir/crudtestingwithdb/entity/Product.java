dbd2abbf6eadcc492082f18a26740804c6a4a050
Takhir <guvalakat1603@gmail.com>
2024-08-12 16:33:12 +0500

    strategy= GenerationType.AUTO is used

--- CRUD-testing-with-db/src/main/java/uz/tokhir/crudtestingwithdb/entity/Product.java
+++ CRUD-testing-with-db/src/main/java/uz/tokhir/crudtestingwithdb/entity/Product.java
@@ -1,9 +1,6 @@
 package uz.tokhir.crudtestingwithdb.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -16,7 +13,7 @@ import lombok.NoArgsConstructor;
 public class Product {
 
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy= GenerationType.AUTO)
     private int id;
     private String name;
     private int quantity;
