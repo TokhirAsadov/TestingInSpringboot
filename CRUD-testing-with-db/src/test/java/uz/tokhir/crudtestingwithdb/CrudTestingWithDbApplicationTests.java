ea051f386051dd7ef71b3905f8d09732a4526851
Takhir <guvalakat1603@gmail.com>
2024-08-12 17:01:03 +0500

    get, create, update methods are tested

--- CRUD-testing-with-db/src/test/java/uz/tokhir/crudtestingwithdb/CrudTestingWithDbApplicationTests.java
+++ CRUD-testing-with-db/src/test/java/uz/tokhir/crudtestingwithdb/CrudTestingWithDbApplicationTests.java
@@ -3,11 +3,19 @@ package uz.tokhir.crudtestingwithdb;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.test.context.jdbc.Sql;
 import org.springframework.web.client.RestTemplate;
+import uz.tokhir.crudtestingwithdb.entity.Product;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+//@SpringBootTest
 class CrudTestingWithDbApplicationTests {
 
     @LocalServerPort
@@ -17,6 +25,9 @@ class CrudTestingWithDbApplicationTests {
 
     private static RestTemplate restTemplate;
 
+    @Autowired
+    private TestH2Repository h2Repository;
+
     @BeforeAll
     public static void init(){
         restTemplate = new RestTemplate();
@@ -25,6 +36,50 @@ class CrudTestingWithDbApplicationTests {
     @BeforeEach
     public void setUp(){
         baseUrl=baseUrl.concat("/products");
+    }
+
+    @Test
+    public void testAddProduct(){
+        Product product = new Product("mouse",2,444);
+        Product response = restTemplate.postForObject(baseUrl, product, Product.class);
+        assertEquals("mouse",response.getName());
+        assertEquals(1,h2Repository.findAll().size());
+    }
+
+    @Test
+    @Sql(statements = "INSERT INTO PRODUCT_TBL (id, name, quantity, price) VALUES (4,'phone',1,1229)",executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD)
+    @Sql(statements = "DELETE FROM PRODUCT_TBL WHERE name='phone'",executionPhase = Sql.ExecutionPhase.AFTER_TEST_METHOD)
+    public void testGetProducts(){
+        List<Product> products = restTemplate.getForObject(baseUrl, List.class);
+        assertEquals(1,products.size());
+        assertEquals(1,h2Repository.findAll().size());
+    }
+
+    @Test
+    @Sql(statements = "INSERT INTO PRODUCT_TBL (id, name, quantity, price) VALUES (1,'notebook',1,119)",executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD)
+    @Sql(statements = "DELETE FROM PRODUCT_TBL WHERE id=1",executionPhase = Sql.ExecutionPhase.AFTER_TEST_METHOD)
+    public void testFindProductById(){
+        Product product = restTemplate.getForObject(baseUrl+"/{id}", Product.class,1);
+        assertAll(
+                ()->assertNotNull(product),
+                ()->assertEquals(1,product.getId()),
+                ()->assertEquals("notebook",product.getName())
+        );
+
+    }
+
+    @Test
+    @Sql(statements = "INSERT INTO PRODUCT_TBL (id, name, quantity, price) VALUES (2,'cup',1,229)",executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD)
+    @Sql(statements = "DELETE FROM PRODUCT_TBL WHERE id=2",executionPhase = Sql.ExecutionPhase.AFTER_TEST_METHOD)
+    public void testUpdateProduct(){
+        Product product = new Product("cup",1,2229);
+        restTemplate.put(baseUrl+"/update/{id}", product,2);
+        Product productFromDB = h2Repository.findById(2).get();
+        assertAll(
+                ()->assertNotNull(product),
+                ()->assertEquals(2229,product.getPrice())
+        );
+
     }
 
 }
