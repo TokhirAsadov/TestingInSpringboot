fa4239e80f2f48862a8112398f7ccb5491a16eb3
Takhir <guvalakat1603@gmail.com>
2024-08-12 16:32:55 +0500

    schema and data url are added

--- CRUD-testing-with-db/src/test/java/uz/tokhir/crudtestingwithdb/CrudTestingWithDbApplicationTests.java
+++ CRUD-testing-with-db/src/test/java/uz/tokhir/crudtestingwithdb/CrudTestingWithDbApplicationTests.java
@@ -1,13 +1,30 @@
 package uz.tokhir.crudtestingwithdb;
 
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.web.client.RestTemplate;
 
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class CrudTestingWithDbApplicationTests {
 
-    @Test
-    void contextLoads() {
+    @LocalServerPort
+    private int port;
+
+    private String baseUrl = "http://localhost:" + port;
+
+    private static RestTemplate restTemplate;
+
+    @BeforeAll
+    public static void init(){
+        restTemplate = new RestTemplate();
+    }
+
+    @BeforeEach
+    public void setUp(){
+        baseUrl=baseUrl.concat("/products");
     }
 
 }
