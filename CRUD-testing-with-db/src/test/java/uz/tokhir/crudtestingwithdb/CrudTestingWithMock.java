3ac47e6d1676fe06617e4f4b8222474589314643
Takhir <guvalakat1603@gmail.com>
2024-08-13 14:58:43 +0500

    testing

--- CRUD-testing-with-db/src/test/java/uz/tokhir/crudtestingwithdb/CrudTestingWithMock.java
+++ CRUD-testing-with-db/src/test/java/uz/tokhir/crudtestingwithdb/CrudTestingWithMock.java
@@ -1,9 +1,22 @@
 package uz.tokhir.crudtestingwithdb;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.aspectj.lang.annotation.Before;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import uz.tokhir.crudtestingwithdb.controller.ProductController;
+import uz.tokhir.crudtestingwithdb.entity.Product;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
 @AutoConfigureMockMvc
@@ -12,5 +25,38 @@ class CrudTestingWithMock {
     @Autowired
     private MockMvc mockMvc;
 
+    @BeforeEach
+    public void setup(){
+        this.mockMvc = MockMvcBuilders
+                .standaloneSetup(ProductController.class)
+                .build();
+    }
 
+    @Test
+    public void addNewProductTest() throws Exception{
+        Product product = Product.builder()
+                .name("test-product-mock")
+                .price(555)
+                .quantity(1)
+                .build();
+
+        mockMvc.perform(MockMvcRequestBuilders
+                .post("/products")
+                .contentType("application/json")
+                .content(asJsonString(product))
+                .accept("application/json"))
+                .andExpect(status().isOk())
+                .andExpect(MockMvcResultMatchers.jsonPath("$.id").exists());
+    }
+
+
+    private String asJsonString(Object obj) {
+        try {
+            return new ObjectMapper().writeValueAsString(obj);
+        }
+        catch (JsonProcessingException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
 }
