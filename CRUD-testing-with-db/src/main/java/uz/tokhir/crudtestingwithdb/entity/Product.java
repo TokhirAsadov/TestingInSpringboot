e7848f01848088936d0e2dc7d89986810f5622f6
Takhir <guvalakat1603@gmail.com>
2024-08-13 14:58:52 +0500

    @builder is used

--- CRUD-testing-with-db/src/main/java/uz/tokhir/crudtestingwithdb/entity/Product.java
+++ CRUD-testing-with-db/src/main/java/uz/tokhir/crudtestingwithdb/entity/Product.java
@@ -2,6 +2,7 @@ package uz.tokhir.crudtestingwithdb.entity;
 
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
@@ -10,6 +11,7 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @Entity
 @Table(name = "PRODUCT_TBL")
+@Builder
 public class Product {
 
     @Id
